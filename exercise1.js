4d768902847b2b58eee6dfe31d59aacf0efedd20
Cvetanka <apostolovacvetanka4@gmail.com>
2024-04-20 21:19:19 +0200

    changed

--- exercise1.js
+++ exercise1.js
@@ -2,11 +2,9 @@
 for a discount based on their age and membership status. */
 
 let age = 20;
-let isMember = false;
+let isMember = true;
 
-const isEligible = (age >= 18 && isMember) || isMember;
-
-if (isEligible) {
+if ((age >= 18 && isMember) || isMember) {
  console.log("Congratulations! You are eligible for the discount.");
 } else {
  console.log("Sorry, you are not eligible for the discount.");
